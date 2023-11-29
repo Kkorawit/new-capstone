007f06ed008781d0034574b9062b2af31aac6166
korawit <korawit-2545_@hotmail.com>
2023-11-29 21:58:40 +0700

    1st commit

---
+++ src/main/java/sit/capstone/vevent/VeventApplication.java
@@ -0,0 +1,13 @@
+package sit.capstone.vevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sit/capstone/vevent/VeventApplicationTests.java
@@ -0,0 +1,13 @@
+package sit.capstone.vevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
